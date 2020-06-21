83218e857d04f50986334a61009d9c710e2705c9
octoriefeisal <muhoctorie@gmail.com>
2020-06-21 21:31:16 +0700

    Tank 54
    Pemograman Simulasi

---
+++ Assets/Script/GerakPeluruScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class GerakPeluruScript : MonoBehaviour {
+
+	private Transform myTransform;
+	public float waktuTerbangPeluru;
+
+	private TankBehaviorScript tankBehavior;
+	private float _kecAwal;
+	private float _sudutMeriam;
+	private float _sudutTembak;
+	private Vector3 _posisiAwal;
+	private AudioSource audioSource;
+
+	public GameObject ledakan;
+	public AudioClip audioLedakan;
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;
+
+		tankBehavior = GameObject.FindObjectOfType<TankBehaviorScript>();
+		_kecAwal = tankBehavior.kecepatanAwalPeluru;
+		_sudutTembak = tankBehavior.nilaiRotasiY;
+		_sudutMeriam = tankBehavior.sudutMeriam;
+
+		_posisiAwal = myTransform.position;
+
+
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		waktuTerbangPeluru += Time.deltaTime;
+
+		myTransform.position = PosisiTerbangPeluru(myTransform.position, _kecAwal, waktuTerbangPeluru, _sudutTembak, _sudutMeriam);
+	}
+
+	private Vector3 PosisiTerbangPeluru(Vector3 _posisiAwal, float _kecAwal, float _waktu, 
+		float _sudutTembak,float  _sudutMeriam)
+    {
+		float _x = _posisiAwal.x + (_kecAwal * _waktu * Mathf.Sin(_sudutMeriam * Mathf.PI / 180));
+		float _y = _posisiAwal.y + ((_kecAwal * _waktu * Mathf.Sin(_sudutTembak * Mathf.PI / 180)) - (0.5f * 10 * Mathf.Pow(_waktu,2)));
+		float _z = _posisiAwal.z +(_kecAwal * _waktu * Mathf.Cos(_sudutMeriam * Mathf.PI / 180));
+
+		return new Vector3(_x,_y,_z);
+    }
+
+	 private void OnTriggerEnter(Collider other)
+    {
+		if( other.tag == "Land1")
+        {
+			//Debug.Log("KENAAA");
+			Destroy(this.gameObject,2f);
+
+			GameObject go = Instantiate(ledakan, myTransform.position,
+				Quaternion.identity);
+			Destroy(go, 3f);
+
+			audioSource.PlayOneShot(audioLedakan);
+        }
+    }
+
+	
+}
---
+++ Assets/Script/TankBehaviorScript.cs
@@ -0,0 +1,135 @@
+﻿using Boo.Lang.Environments;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankBehaviorScript : MonoBehaviour {
+	
+	private Transform myTransform;
+	private GameObject selongsong;
+	private GameObject titikTembakan;
+	private AudioSource audioSource;
+	private string stateRotasiVertikal;
+
+	
+
+	public float kecepatanRotasi = 20;
+	public float kecepatanAwalPeluru = 20;
+
+	[HideInInspector]
+	public float sudutMeriam;
+
+	[HideInInspector]
+	public float nilaiRotasiY;
+
+	public GameObject objekTembakan;
+	public GameObject objekLedakan;
+	public GameObject peluruMeriam;
+	public AudioClip audioTembakan;
+	public AudioClip audioLedakan;
+	
+	
+	// Use this for initialization
+	
+	void Start () {
+		myTransform = transform;
+		
+		//inisialisasi selongsong
+		selongsong = myTransform.Find("selongsong").gameObject;
+		
+		//inisialisasi titk tembakan
+		titikTembakan = selongsong.transform.Find("titiktembakan").gameObject;
+
+		//inisialisasi komponen audiosource
+		audioSource = selongsong.GetComponent<AudioSource>();
+	}
+	
+	void Update(){
+		//rotasi horizontal
+		if(Input.GetKey(KeyCode.T)) //horizontal lawan arah
+		{
+			myTransform.Rotate(Vector3.back * kecepatanRotasi * Time.deltaTime, Space.Self);
+
+			
+		}	
+		else if(Input.GetKey(KeyCode.U)) //horizontal searah jam
+
+		{
+			myTransform.Rotate(Vector3.forward * kecepatanRotasi * Time.deltaTime, Space.Self);
+		}
+
+		sudutMeriam = myTransform.localEulerAngles.z;		
+		//region menentukan state
+		nilaiRotasiY= 360-selongsong.transform.localEulerAngles.x;
+		
+		if (nilaiRotasiY ==0 ||nilaiRotasiY==360)
+		{
+			stateRotasiVertikal="aman";
+		}
+		else if (nilaiRotasiY > 0 && nilaiRotasiY<15)
+		{
+			stateRotasiVertikal="aman";
+		}
+		else if (nilaiRotasiY > 15 && nilaiRotasiY <16)
+		{
+			stateRotasiVertikal="atas";
+		}
+		else if (nilaiRotasiY > 350)
+		{
+			stateRotasiVertikal = "bawah";
+		}
+		
+		//region arah rotasi vertikal berdasarkan state
+		if( stateRotasiVertikal=="aman")
+		{
+			if(Input.GetKey(KeyCode.Y))
+			{
+				selongsong.transform.Rotate( Vector3.left * kecepatanRotasi * Time.deltaTime, Space.Self);
+			}
+			else if(Input.GetKey(KeyCode.H))
+			{
+				selongsong.transform.Rotate( Vector3.right * kecepatanRotasi * Time.deltaTime, Space.Self);
+			}
+		}
+		else if( stateRotasiVertikal=="bawah")
+		{
+			selongsong.transform.localEulerAngles = new Vector3(-0.5f, selongsong.transform.localEulerAngles.y, selongsong.transform.localEulerAngles.z);
+			                                              
+		}
+		else if( stateRotasiVertikal=="atas")
+		{
+			selongsong.transform.localEulerAngles = new Vector3(-14.5f, selongsong.transform.localEulerAngles.y, selongsong.transform.localEulerAngles.z);
+			                                              
+		}
+		
+		
+		//region Penembakan
+		if( Input.GetKeyDown(KeyCode.Space))
+		{
+			#region Init Peluru
+			 GameObject peluru = Instantiate(peluruMeriam, titikTembakan.transform.position,
+			                                     Quaternion.Euler(
+												 selongsong.transform.localEulerAngles.x,
+			                                     myTransform.localEulerAngles.z,
+			                                     0)); 
+			#endregion
+
+			#region Init Objek tembakan
+			GameObject efekTembakan = Instantiate(objekTembakan, titikTembakan.transform.position,
+				Quaternion.Euler(
+					selongsong.transform.localEulerAngles.x,
+					myTransform.localEulerAngles.z, 0));
+			Destroy(efekTembakan, 1f) ;
+			#endregion
+
+			#region Init audio objek tembakan
+			audioSource.PlayOneShot(audioTembakan);
+
+            #endregion
+        }
+
+	}
+
+}
+
+	
